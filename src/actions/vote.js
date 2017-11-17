7ef342d7a9c2ac7242e68d8d6b85086c9e30e413
Evan Kysley <evan@kysley.com>
2017-11-17 17:09:47 -0500

    RUNNIN GOUT OF TIME

--- src/actions/vote.js
+++ src/actions/vote.js
@@ -2,12 +2,12 @@ export const SUBMIT_VOTE_REQUEST = 'SUBMIT_VOTE_REQUEST'
 export const SUBMIT_VOTE_SUCCESS = 'SUBMIT_VOTE_SUCCESS'
 export const SUBMIT_VOTE_FAILURE = 'SUBMIT_VOTE_FAILURE'
 
-export const submiteVoteRequest = () => ({
+export const submitVoteRequest = () => ({
   type: SUBMIT_VOTE_REQUEST,
 })
 
-export const submitVoteSuccess = ({ id }) => {
-  localStorage.setitem(id, 'voted')
+export const submitVoteSuccess = (id, voteType) => {
+  localStorage.setItem(id, voteType)
 
   return {
     type: SUBMIT_VOTE_SUCCESS,
@@ -15,11 +15,8 @@ export const submitVoteSuccess = ({ id }) => {
   }
 }
 
-export const submitVoteFailure = ({ id }) => {
-  localStorage.removeItem(id)
-
-  return {
-    type: SUBMIT_VOTE_FAILURE,
-    voteSuccess: false,
-  }
-}
+export const submitVoteFailure = () => ({
+  // localStorage.removeItem(id)
+  type: SUBMIT_VOTE_FAILURE,
+  voteSuccess: false,
+})
