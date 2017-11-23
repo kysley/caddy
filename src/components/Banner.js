2554169e6b72d538c047cf43e361402282fd87ad
Evan Kysley <evan@kysley.com>
2017-11-23 15:02:09 -0500

    update banner styles+content

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Banner = () => (
-  <header className="banner fadeIn delay-2">
+  <header className="banner fadeIn dur-3">
     <h1 className="banner--primary">Making decisions is hard...</h1>
-    <h2 className="banner--secondary"><span className="issy--span">issy</span> helps you make it easier than ever 🎉</h2>
+    <h2 className="banner--secondary"><span className="issy--span">issy</span> helps you get a yes/no answer
+      faster than ever! <span role="img" aria-label="tada!">🎉</span>
+    </h2>
+    <h3 className="banner--disclaimer">(unless your friends are just as indecisive as you)</h3>
   </header>
 )
 
